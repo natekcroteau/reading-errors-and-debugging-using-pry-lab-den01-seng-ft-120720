bb92b17a97c71e909778bb0f613f2a4fb0df4def
Nate Croteau <natekcroteau@gmail.com>
2020-11-15 17:00:20 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -14,12 +14,10 @@ end
 
 def selection(num)
   if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
+     puts "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
+     puts "HAM HAM HAM IN MY TUMMY"
   end
-  binding.pry
-  puts"prying"
 end
 
 def runner
