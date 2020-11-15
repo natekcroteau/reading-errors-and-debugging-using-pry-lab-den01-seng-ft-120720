f58e106baf48a63313c85348342a83e79095b8a3
Nate Croteau <natekcroteau@gmail.com>
2020-11-15 17:25:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -11,6 +11,8 @@ def state_log(star_date)
 end
 
 def engage
-  puts state_log(star_date)
-  date = generate_star_date
+  puts state_log(date)
+    date = generate_star_date
+    binding.pry
+    puts "hello"
 end
