51e1f6ffc175a687682d525c44d9e9b745eadf90
Nate Croteau <natekcroteau@gmail.com>
2020-11-15 17:16:36 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,10 +1,11 @@
 # don't forget to add: require 'pry'
+
 require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
     10 * "s" + string
-    binding.pry
+  binding.pry
     puts "w"
   else
     string
